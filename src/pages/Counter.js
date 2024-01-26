f51e75a0e505217d5b819f5bb8d3a319eb8691b1
Rithanya N <125180915+RithanyaN@users.noreply.github.com>
2024-01-26 14:35:53 +0530

    Create Counter.js

---
+++ src/pages/Counter.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./Counter.css";
+const CounterComponent = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
+  const resetCount = () => {
+    setCount(0);
+  };
+
+  return (
+    <div>
+      <div className="Container">
+        <div className="counter-box">
+         <center><h1>Counter: {count}</h1></center>
+          <div className="button-container">
+            <button onClick={incrementCount}>Increment</button>
+            <button onClick={decrementCount}>Decrement</button>
+            <button onClick={resetCount}>Reset</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default function Router() {
+  return <CounterComponent />;
+}
