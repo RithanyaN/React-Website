d73867fd12aa6c9e005b260bdb1a2e98cd3f7268
Rithanya N <125180915+RithanyaN@users.noreply.github.com>
2024-01-26 14:38:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
 import Service from "./pages/Service"
+import Counter from "./pages/Counter.js"
+import Effect from "./pages/Effect.js"
+import Reducer from "./pages/Reducer.js"
 
 export default function App() {  
   return (
@@ -14,9 +17,11 @@ export default function App() {
     <Route path="about" element={<About/>} />
     <Route path="contact" element={<Contact/>} />
     <Route path="service" element={<Service/>} />
-    
+    <Route path="counter" element={<Counter/>} />
+    <Route path="effect" element={<Effect/>} />
+    <Route path="reducer" element={<Reducer/>} />
     </Route>
     </Routes>
     </BrowserRouter>
-  )
+  );
 } 
